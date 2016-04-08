a6414c580039dc9b14f7ef3344c661d9233a213c
sai.dandem <sai.dandem@admin-PC.gateway>
2016-04-08 15:14:28 +1000

    Initial Commit.

---
+++ raspi-labs/src/main/java/com/pi4j/raspi/labs/Main.java
@@ -0,0 +1,12 @@
+package com.pi4j.raspi.labs;
+
+/**
+ * Main class of the raspi-labs.
+ *
+ * @author sai.dandem
+ */
+public class Main {
+    public static void main(String... a) {
+        System.out.println("Hello Raspi...");
+    }
+}
